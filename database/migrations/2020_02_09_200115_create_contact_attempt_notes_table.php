d5066b6bbf7d1cbc9bcfbfabfc0155d3737e47c6
ginnac <ginnacampbell1990@gmail.com>
2020-02-09 14:44:29 -0700

    new migration for contact_attempt_notes_table

---
+++ database/migrations/2020_02_09_200115_create_contact_attempt_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactAttemptNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_attempt_notes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('contact_request_id');
+            $table->text('Notes');
+            $table->text('by_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_attempt_notes');
+    }
+}
