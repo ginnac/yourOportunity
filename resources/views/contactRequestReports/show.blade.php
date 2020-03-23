02e2efa9bf34a57388ec2aa27ad1495163624f50
ginnac <ginnacampbell1990@gmail.com>
2020-03-23 17:34:54 -0600

    changes to show details of prospect view

--- resources/views/contactRequestReports/show.blade.php
+++ resources/views/contactRequestReports/show.blade.php
@@ -21,33 +21,33 @@
                     </div>
                     <div class="row">
                         <div class="col">
-                            <h3>Prospect Details</h3>
-                             <table class="table">
+                            <h3 class="mt-5">Prospect Details</h3>
+                             <table class="table table-responsive-sm">
+                             <thead class="thead-dark">
+                                    <tr>
+                                        <th scope="col">Name </th>
+                                        <th scope="col">ID#</th>  
+                                        <th scope="col">Phone Number</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col" class="">Prospect's Initial Message</th>
+                                        <th scope="col">Source</th>
+                                        <th scope="col">Created on</th>
+                                        <th scope="col">Updated on</th>
+                                    </tr>
+                                </thead>
+
                                 <tr>
-                                    <td class="text-white-50">Name: <p class="text-white">{{$report->name}}</p></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-white-50">ID#: {{$report->id}}</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-white-50">Phone Number: <p class="text-white">{{$report->phone_number}}</p></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-white-50">Email:<p class="text-white">{{$report->email}}</p></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-white-50">Prospect's Initial Message: <p class="text-white">{{$report->comments}}</p></td>
-                                </tr>
-                                <tr>
-                                    <td class="text-white-50">Created by: <p class="text-white"> {{$report->source}}</p></td>
-                                </tr>
-                                <tr>
-                                    <!-- <td>Created on/Last updated on: {{($report->created_at)->format('d/m/Y')}}/{{$report->updated_at}}</td> -->
+                                    <td> <p class="text-white">{{$report->name}}</p></td>
+                                    <td class="text-white">{{$report->id}}</td>
+                                    <td><p class="text-white">{{$report->phone_number}}</p></td>
+                                    <td><p class="text-white">{{$report->email}}</p></td>
+                                    <td class="text-white-50"><p>{{$report->comments}}</p></td>
+                                    <td class="text-white-50"><p>Created by {{$report->source}}</p></td>
+                                     <!-- <td>Created on/Last updated on: {{($report->created_at)->format('d/m/Y')}}/{{$report->updated_at}}</td> -->
                                     <td class="text-white-50" >Created on {{($report->created_at)->format('l jS \\of F Y h:i:s A')}}</td>
-                                </tr>
-                                <tr>
                                     <td class="text-white-50">Last update on {{($report->updated_at)->format('l jS \\of F Y h:i:s A')}}</td>
                                 </tr>
+                            
                              </table>
 
                         </div>
@@ -55,13 +55,14 @@
                     <div class="row mt-5">
                         <div class="col">
                            <h3>Contact Notes</h3>
-                            <table class="table">
-
+                            <table class="table table-hover table-responsive-sm">
+                                <thead class="thead-dark">
                                     <tr>
                                         <th>Note</th>
                                         <th>Entered by</th>
                                         <th>Created on</th>
                                     </tr>
+                                </thead>
                                 @foreach($report->contactAttemptNotes as $contactAttemptNote)
                                     <tr>
                                         <td class="text-white">{{$contactAttemptNote->Notes}}</td>
