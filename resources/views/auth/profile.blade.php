e735de62a5053d0e67dd3febc9e5f969ebe7588b
ginnac <ginnacampbell1990@gmail.com>
2020-04-18 13:21:32 -0600

    Adjusted profile view

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Profile</div>
+                    <div class="card-header bg-dark text-white">Profile</div>
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
@@ -47,13 +47,13 @@
                                         <div class="form-group row">
                                             <label for="profile_image" class="col-md-4 col-form-label text-md-right">Profile Image</label>
                                             <div class="col-md-6">
-                                                <input id="profile_image" type="file" class="form-control" name="profile_image">
+                                                <input id="profile_image" type="file" class="form-control-file" name="profile_image">
                                                 @if (auth()->user()->profile_image)
                                                     <code>{{ auth()->user()->profile_image}}</code>
                                                 @endif
                                             </div>
                                         </div>
-                                        <div class="form-group row mb-0 mt-5">
+                                        <div class="form-group row mb-0 mt-2">
                                             <div class="col-md-8 offset-md-4">
                                                 <button type="submit" class="btn btn-primary">Update Profile</button>
                                             </div>
