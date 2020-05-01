30e50e6fb05a3392e2e81282993916eff7a1d6b6
ginnac <ginnacampbell1990@gmail.com>
2020-04-30 21:58:05 -0600

    chamges to show and add new note - Views

--- resources/views/contactAttemptNotes/create.blade.php
+++ resources/views/contactAttemptNotes/create.blade.php
@@ -4,14 +4,9 @@
 
 <div class="title m-b-md mt-5 mb-5">
 
-                    <div class="row">
+                    <div class="row mt-3 mb-3">
                         <div class="col">
-                            <h1 class="float-right text-white-50 font-weight-bold">New Note for {{$report->name}} <i class="fa fa-sticky-note"></i></h1>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col d-flex justify-content-end mt-3 mb-3">
-                            <a class='btn btn-dark' href="/contact_request_reports/{{$report->id}}">Back To Contact Details <i class="fa fa-id-card"></i></a>
+                            <h1 class="float-right text-white font-weight-bold">New Note for {{$report->name}} <i class="fa fa-sticky-note text-white-50"></i></h1>
                         </div>
                     </div>
                     <div class="row">
@@ -31,8 +26,8 @@
                                 <form action="/contact_request_reports/{{$report->id}}/notes" method="POST">
                                     @csrf
                                     <div class="form-group">
-                                        <label class="font-weight-bold" for="Notes">Enter Note:</label>
-                                        <textarea type="text" value="{{old('Notes')}}" class=form-control id="Notes" name="Notes" placeholder="Type Notes"> </textarea>
+                                        <label class="font-weight-bold mb-3" for="Notes">Enter New Note:</label>
+                                        <textarea type="text" value="{{old('Notes')}}" class="form-control" id="Notes" name="Notes" placeholder="Type Notes"> </textarea>
 
                                         <!-- <label for="by_user">User Name:</label>
                                         <input type="text" value="{{old('by_user')}}" class=form-control id="by_user" name="by_user" placeholder="User Name"> -->
@@ -40,7 +35,21 @@
                                         <input type="text" class="form-control" id="by_user" name="by_user" value="{{Auth::user()->name}}" hidden> Created by {{ Auth::user()->name }} </input>
 
                                     </div>
-                                    <button type="submit" class="btn btn-success font-weight-bold btnsize"><i class="fa fa-save"></i> Save</button>
+                                    
+                                    <div class="row">
+                                        <div class="col">
+                                            <a class='btn btn-dark' href="/contact_request_reports/{{$report->id}}">Back <i class="fa fa-id-card"></i></a>
+                                   
+                                        </div>
+                                        <div class="col">
+                                            <button type="submit" class="btn btn-success font-weight-bold btnsize float-right"><i class="fa fa-save"></i> Save</button>
+                                        </div>
+                                    
+                                    </div>
+
+                                    
+                                   
+                                    
                                 </form>
                             </table>
                         </div>
