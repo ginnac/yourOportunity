45d5050ccb8330bc39622662a48074e2073b7045
ginnac <ginnacampbell1990@gmail.com>
2020-05-01 20:58:37 -0600

    changes to add new contact view

--- resources/views/contactRequestReports/create.blade.php
+++ resources/views/contactRequestReports/create.blade.php
@@ -5,15 +5,14 @@
 <div class="title m-b-md mt-4 mb-5">
 
                     <div class="row">
-                        <div class="col">
-                            <h1 class="float-right text-white-50 font-weight-bold">Add New Contact <i class="fa fa-user-plus"></i></h1>
+                        <div class="col-md-11 col-xs-12">
+                            <h1 class="float-right text-white-50 font-weight-bold">Add New Contact <i class="fa fa-user-plus text-white"></i></h1>
+                        </div>
+                        <div class="col-md-1 col-xs-12 mt-1">
+                            <a class='btn btn-dark float-right' href="/contact_request_reports"><i class="fa fa-home"></i></a>
                         </div>
                     </div>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <a class='btn btn-dark float-right' href="/contact_request_reports"><i class="fa fa-home"></i> Home</a>
-                        </div>
-                    </div>
+            
                     <div class="row mt-4">
                         <div class="col">
                             @if($errors->any())
@@ -30,30 +29,50 @@
                             <table class="table mt-2 mb-3">
                                 <form action="/contact_request_reports" method="POST">
                                     @csrf
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-xs-2 col-md-1">
-                                                <label class="font-weight-bold" for="name">Name:</label>
-                                            </div>
-                                            <div class="col-xs-10 col-md-11">
-                                                <input type="text" value="{{old('name')}}" class="form-control mb-3" id="name" name="name" placeholder="Type Contact Name">
-                                            </div>
+                                    
+                                    <div class="form-group row">
+                                        <div class="flex-column pr-3 pl-4">
+                                            <label class="font-weight-bold" for="name">Name:</label>
                                         </div>
-                                        
-                                        
-                                        
-                                        <label class="font-weight-bold" for="email">Email:</label>
-                                        <input type="email" value="{{old('email')}}" class="form-control mb-3" id="email" name="email" placeholder="Type Contact Email">
-
-                                        <label class="font-weight-bold" for="phone_number">Phone Number:</label>
-                                        <input type="number" value="{{old('phone_number')}}" class="form-control mb-3" id="phone_number" name="phone_number" placeholder="Type Contact Phone Number">
-
-                                        <label  class="font-weight-bold" for="comments">Comments:</label>
-                                        <input type="text" value="{{old('comments')}}" class="form-control mb-3" id="comments" name="comments" placeholder="Type Comments">
-                                        
-                                        <input type="text" class="form-control mb-3" id="source" name="source" value="{{Auth::user()->name}}" hidden> Created by {{ Auth::user()->name }} </input>
+                                        <div class="flex-column flex-fill pr-3">
+                                            <input type="text" value="{{old('name')}}" class="form-control mb-3" id="name" name="name" placeholder="Type Contact Name">
+                                        </div>
+                                            
                                     </div>
-                                    <button type="submit" class="btn btn-primary mt-2 font-weight-bold btnsize"><i class="fa fa-save"></i> Save</button>
+
+                                    <div class="form-group row">
+                                        <div class="flex-column pr-3 pl-4">
+                                            <label class="font-weight-bold" for="email">Email:</label>
+                                        </div>
+                                        <div class="flex-column flex-fill pr-3">
+                                            <input type="email" value="{{old('email')}}" class="form-control mb-3" id="email" name="email" placeholder="Type Contact Email">
+                                        </div>   
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="flex-column pr-3 pl-4">
+                                            <label class="font-weight-bold" for="phone_number">Phone Number:</label>
+                                        </div>
+                                        <div class="flex-column flex-fill pr-3">
+                                            <input type="number" value="{{old('phone_number')}}" class="form-control mb-3" id="phone_number" name="phone_number" placeholder="Type Contact Phone Number">
+                                        </div>   
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="flex-column pr-3 pl-4">
+                                            <label  class="font-weight-bold" for="comments">Comments:</label>
+                                        </div>
+                                        <div class="flex-column flex-fill pr-3">
+                                            <input type="text" value="{{old('comments')}}" class="form-control mb-3" id="comments" name="comments" placeholder="Type Comments"> 
+                                        </div>   
+                                    </div>
+
+                                    <div class="form-group row w-100">
+                                        <div class="col">
+                                            <input type="text" class="form-control mb-3" id="source" name="source" value="{{Auth::user()->name}}" hidden> <p class="pl-4 d-flex justify-content-end">Created by {{ Auth::user()->name }}</p> </input>
+                                        </div>
+                                    </div>  
+                                    <button type="submit" class="btn btn-success mt-2 mr-4 font-weight-bold btnsize float-right"><i class="fa fa-save"></i> Save</button>
                                 </form>
                             </table>
                         </div>
--- resources/views/contactRequestReports/edit.blade.php
+++ resources/views/contactRequestReports/edit.blade.php
@@ -4,15 +4,16 @@
 
 <div class="title m-b-md mt-4 mb-5">
 
-                    <div class="row">
-                        <div class="col">
-                            <h1 class="float-right text-white-50 font-weight-bold">Edit Contact Details for {{$report->name}} <i class="fa fa-edit"></i></h1>
+                    <div class="row mt-5 mb-5">
+                        <div class="col-md-11 col-xs-12">
+                            <h1 class="float-right text-white-50 font-weight-bold">Edit Contact Details for {{$report->name}} <i class="fa fa-edit text-white"></i></h1>
+                        </div>
+                        <div class="col-md-1 col-xs-12 mt-1">
+                            <a class='btn btn-dark float-right' href="/contact_request_reports"><i class="fa fa-home"></i></a>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col d-flex justify-content-end mt-3 mb-3">
-                            <a class='btn btn-dark' href="/contact_request_reports"><i class="fa fa-home"></i> Home</a>
-                        </div>
+                        
                     </div>
                     <div class="row">
                         <div class="col">
@@ -20,19 +21,34 @@
                                 <form action="/contact_request_reports/{{$report->id}}" method="POST">
                                     @csrf
                                     @method('put')
-                                    <div class="form-group">
-                                        <label class="font-weight-bold" for="name">Name:</label>
-                                        <input type="text" value="{{$report->name}}" class=form-control id="name" name="name" placeholder="Enter Contact's Name">
+                                    <div class="form-group row">
+                                        <div class="flex-column pr-3 pl-4">
+                                            <label class="font-weight-bold mr-1" for="name">Name:</label>
+                                        </div>
+                                        <div class="flex-column flex-fill pr-3">
+                                            <input type="text" value="{{$report->name}}" class=form-control id="name" name="name" placeholder="Enter Contact's Name">
+                                        </div>
                                     </div>
-                                    <div class="form-group">
-                                        <label class="font-weight-bold" for="phone_number">Phone Number:</label>
-                                        <input type="text" value="{{$report->phone_number}}" class=form-control id="phone_number" name="phone_number" placeholder="Enter Contact's Phone Number">
+                                    <div class="form-group row">
+                                        <div class="flex-column pr-3 pl-4">
+                                            <label class="font-weight-bold mr-1" for="phone_number">Phone Number:</label>
+                                        </div>
+                                        <div class="flex-column flex-fill pr-3">
+                                            <input type="text" value="{{$report->phone_number}}" class=form-control id="phone_number" name="phone_number" placeholder="Enter Contact's Phone Number">
+                                        </div>
+                                        
+                                        
                                     </div>
-                                    <div class="form-group">
-                                        <label class="font-weight-bold" for="email">Email:</label>
-                                        <input type="text" value="{{$report->email}}" class=form-control id="email" name="email" placeholder="Enter Contact's Email">
+                                    <div class="form-group row">
+                                        <div class="flex-column pr-3 pl-4">
+                                            <label class="font-weight-bold" for="email">Email:</label>
+                                        </div>
+                                        <div class="flex-column flex-fill pr-3">
+                                            <input type="text" value="{{$report->email}}" class=form-control id="email" name="email" placeholder="Enter Contact's Email">
+                                        </div>
                                     </div>
-                                    <button type="submit" class="btn btn-primary mt-2 font-weight-bold btnsize"><i class="fa fa-save"></i> Save</button>
+
+                                    <button type="submit" class="btn btn-success mt-2 font-weight-bold btnsize float-right"><i class="fa fa-save"></i> Save</button>
                                 </form>
                             </table>
                         </div>
